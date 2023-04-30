1ea8e0a0ee134e28ac6314e775b93520cbd7bc56
vlad <s02200473@gse.cs.msu.ru>
2023-04-30 14:39:55 +0300

    Complete web tests using Selenium

---
+++ src/main/resources/sql_scripts/insert_values_webtest.sql
@@ -0,0 +1,34 @@
+INSERT INTO Clients(full_name, phone, email, preferred_city) VALUES
+    ('Гришин Михаил Дмитриевич', '89846241738', 'gmd@mail.ru', 'Москва'),
+    ('Смирнова Василиса Денисовна', '89409655768', 'svden@yandex.ru', 'Воронеж'),
+    ('Богданова Вероника Евгеньевна', '89799794197', NULL, 'Москва')
+;
+
+INSERT INTO Airports(iata_code, airport_name, airport_country, airport_city) VALUES
+    ('VKO', 'Международный аэропорт Внуково', 'Россия', 'Москва'),
+    ('OVB', 'Международный аэропорт Толмачёво', 'Россия', 'Новосибирск'),
+    ('LED', 'Международный аэропорт Пулково', 'Россия', 'Санкт-Петербург'),
+    ('DME', 'Международный аэропорт Домодедово', 'Россия', 'Москва'),
+    ('IST', 'Международный аэропорт Стамбул', 'Турция', 'Стамбул')
+;
+
+INSERT INTO Airlines(airline_name, registration_country, airline_phone, airline_email, airline_website) VALUES
+    ('S7 Airlines', 'Россия', '84957830707', NULL, 'www.s7.ru'),
+    ('Аэрофлот', 'Россия', '88004445555', NULL, 'www.aeroflot.ru'),
+    ('Победа', 'Россия', '88095054777', '', 'www.pobeda.aero'),
+    ('Turkish Airlines', 'Турция', NULL, NULL, 'www.turkishairlines.com'),
+    ('Emirates', 'ОАЭ', NULL, NULL, 'www.emirates.com')
+;
+
+INSERT INTO Flights(airline_id, departure_airport_id, arrival_airport_id, departure_time, arrival_time,
+                    current_price, available_seats, total_seats) VALUES
+    (1, 4, 2, '2023-02-28 11:15', '2023-02-28 15:05', 8200, 10, 200),
+    (4, 5, 4, '2023-04-14 10:00', '2023-04-14 16:30', 35000, 7, 250),
+    (2, 5, 3, '2023-03-03 14:45', '2023-03-03 21:35', 30000, 0, 175)
+;
+
+INSERT INTO TICKETS(client_id, flight_id, purchase_price, purchase_time) VALUES
+    (1, 3, 33500, '2023-02-02 16:04'),
+    (2, 2, 4500, '2023-02-26 15:12'),
+    (2, 1, 8000, '2023-02-03 14:48')
+;
